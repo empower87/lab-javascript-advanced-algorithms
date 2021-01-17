aae8fd0795b5b9f88da2337b6e86fcc254762280
empower87 <mjamespower87@gmail.com>
2021-01-16 18:18:43 -0800

    half way

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -1,26 +1,42 @@
 class StackDataStructure {
-  constructor() {
+  constructor(item) {
     this.stackControl = [];
     this.MAX_SIZE = 10;
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < 10) {
+      return this.stackControl
+      console.log(stackControl.length)
+    }
+    else {     
+      return [false, "Stack Overflow"]
+    }
   }
 
   display() {
+    return this.stackControl[this.stackControl.length - 1]
     // ... your code goes here
   }
 
   isEmpty() {
+    if (this.stackControl.length === 0) {
+      return true
+    }
+    else {
+      return false
+    }
     // ... your code goes here
   }
 
   push(item) {
+      this.stackControl.push(item)
+      return this.stackControl
     // ... your code goes here
   }
 
   pop() {
+    this.stackControl.pop()
     // ... your code goes here
   }
 }
